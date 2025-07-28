cf3da3953e28ca1b53ff642cd862e22c2305e0d1
sh07oo <sh630hoo@gmail.com>
2025-07-28 23:51:19 +0900

    Create server_main.py

---
+++ server/server_main.py
@@ -0,0 +1,19 @@
+import os
+from notification import Notification
+
+if __name__ == "__main__":
+    EMAIL = os.environ.get("EMAIL")
+    PASSWD = os.environ.get("PASSWD")
+
+    gallery_url = "https://gall.dcinside.com/mgallery/board/lists?id=seunjae"  # 원하는 갤러리 주소
+    keyword_list = none
+
+    notifier = Notification(
+        gallery_url=gallery_url,
+        use_desktop=False,
+        use_mobile=True,
+        email=EMAIL,
+        password=PASSWD,
+        keywords=keyword_list
+    )
+    notifier.run()
